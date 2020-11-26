d5943e6bc4572ea6a8b9a9b03a6b7e3f058b4853
ayushgupta258 <36157834+ayushgupta258@users.noreply.github.com>
2020-11-27 00:22:59 +0530

    Support different delimiter

--- Calc.java
+++ Calc.java
@@ -4,10 +4,23 @@ public class Calc {
 			return 0;
 		}
 		else {
+			String[] nums=null;
+			if(number.startsWith("//")){
+				String[] customDelimatorWithNumber=number.split("\n",2);
+				String delimator=customDelimatorWithNumber[0];
+				number=customDelimatorWithNumber[1].trim();
+				nums=number.split(delimator.substring(2));
+			}
+			else {
+				nums=number.split(",|\n");
+			}
 			int sum=0;
-			String[] nums=number.split(",|\n");
 			for(String num: nums) {
-				sum+=Integer.parseInt(num);
+				try {
+					sum+=Integer.parseInt(num);
+				}catch(Exception e) {
+					continue;
+				}
 			}
 			return sum;
 		}
